207539f9935db9184d4e306ad502143f3632a733
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2025-03-27 12:41:15 +0000

    FEAT: Created simple website layout

--- services/sudoku-solver/project/server.py
+++ services/sudoku-solver/project/server.py
@@ -1,7 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def redirect_to_sudoku():
+    return redirect("/sudoku")
+
+
+@app.route("/sudoku")
+def sudoku():
+    """Provides an interface for inputting a sudoku puzzle and getting solution"""
+    return render_template('sudoku.html')
+
+
+@app.route("/saved-puzzles")
+def saved_puzzles():
+    """Provides a list of saved puzzles"""
+    return render_template('saved-puzzles.html')
