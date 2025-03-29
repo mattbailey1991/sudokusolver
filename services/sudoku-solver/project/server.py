e32b45f45568af9866e359001217836e634e8ba9
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2025-03-29 18:29:47 +0000

    FEAT: Connected app to MongoDB and added ability to save sudoku grids

--- services/sudoku-solver/project/server.py
+++ services/sudoku-solver/project/server.py
@@ -1,7 +1,21 @@
 from flask import Flask, jsonify, render_template, redirect, request, Response
+import pymongo
 from project.sudoku import *
 
 app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(host="mongodb", 
+                                port=27017, 
+                                serverSelectionTimeoutMS = 1000,
+                                username = "admin",
+                                password = "password")
+    print("Successfully connected to MongoDB")
+
+except:
+    raise RuntimeError("Cannot connect to MongoDB. If using local host, try: docker run mongo")
+
+db = mongo.saved_grids
 
 @app.route("/")
 def redirect_to_sudoku():
@@ -17,9 +31,9 @@ def sudoku():
     
     # Return solved sudoku
     if request.method == "POST":
-        data = request.get_json()
-        print(data)
-        sudoku = Sudoku(data, file_type="dict")
+        grid_data = request.get_json()
+        print(grid_data)
+        sudoku = Sudoku(grid_data, file_type="dict")
         result = sudoku.solve()
         return jsonify({"solution": result})
 
@@ -28,3 +42,21 @@ def sudoku():
 def saved_puzzles():
     """Provides a list of saved puzzles"""
     return render_template('saved-puzzles.html')
+
+
+@app.route("/save-puzzle", methods = ["POST"])
+def save_puzzle():
+    """Saves a puzzle to sudokus collection"""
+    try:
+        req_data = request.get_json()
+        name = req_data["name"]
+        grid_data = req_data["grid_data"]
+        if db.sudokus.find_one({"name": name}):
+            return jsonify({"message": "Name already in use"})
+        else:
+            dbResponse = db.sudokus.insert_one({"name": name, "grid_data": grid_data})
+            print(dbResponse.inserted_id)
+            return jsonify({"message": "Sudoku saved"})
+    
+    except:
+        raise RuntimeError("Could not save sudoku data to data")
--- services/sudoku-solver/project/static/sudoku.js
+++ services/sudoku-solver/project/static/sudoku.js
@@ -1,14 +1,60 @@
 // Sends puzzle to server
 $(document).ready(function(){
 
-    // 
+    // sudoku-form submission
+    // Sends the grid to the server using AJAX, gets a solution using AC3 and backtrack, and updates page with solution 
     $('#sudoku-form').submit(function (event) {
         event.preventDefault();
-        
+        var sudoku = grid_data()
+        console.log(JSON.stringify(sudoku))
+
+        // Post the data to /sudoku route using AJAX to obtain solution
+        $.ajax({
+            type: 'POST',
+            url: "",
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            data: JSON.stringify(sudoku),
+            success: function(response) {
+                solution = JSON.parse(response["solution"]);
+                update_page(solution);
+            }
+        });
+    });
+
+
+    // save-form submission
+    // Sends the grid to the server and saves to a mongodb database
+    $('#save-form').submit(function (event) {
+        event.preventDefault();
+
+        // Get sudoku grid data
+        var sudoku = grid_data()
+        console.log(JSON.stringify(sudoku))
+
+        // Get save name
+        var name = document.getElementById('sudoku-name').value
+
+        // Posts the grid data to save-form route using AJAX
+        $.ajax({
+            type: "POST",
+            url: "/save-puzzle",
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            data: JSON.stringify({"name":name, "grid_data":sudoku}),
+            success: function(response) {
+                var message = response["message"];
+                $('#message-area').html('<div class="container-fluid px-0"> <div class="alert alert-info" role="alert">' + message + '</div> </div>');
+            }
+        });
+    });
+
+
+    function grid_data() {
         // Create a list of inputs
         var inputs = document.getElementsByTagName('input'),
         input_list = {};
-        for (var i = 0; i < inputs.length-1; i++) {
+        for (var i = 0; i < inputs.length-3; i++) {
             input_list[i] = inputs[i].value;
         }
         
@@ -24,23 +70,12 @@ $(document).ready(function(){
             let column = k % 9;
             sudoku[row][column] = parseInt(v);
         }
-        console.log(JSON.stringify(sudoku))
 
-        // Post the data to server using AJAX and obtain solution
-        $.ajax({
-            type: 'POST',
-            url: "",
-            contentType: 'application/json;charset=UTF-8',
-            dataType: 'json',
-            data: JSON.stringify(sudoku),
-            success: function(response) {
-                solution = JSON.parse(response["solution"]);
-                update_page(solution);
-            }
-        });
-    });
+        return sudoku;
+    }
 
-    function update_page(solution) {
+    // Updates a page with a grid
+    function update_page(grid) {
         if (solution == 9999) {
             console.log("No solution")
         }
